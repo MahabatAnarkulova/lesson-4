c3969e01842a6c888511c68285c88729509404a3
MahabatAnarkulova <anarkulovamahabat18@gmail.com>
2023-10-27 10:38:51 +0600

    homework

---
+++ main.js
@@ -0,0 +1,32 @@
+// 1) Напишите программу, где пользователь вводит любое целое положительное число. А программа суммирует все числа от 1 до введенного пользователем числа.
+
+// let userInput = parseInt(prompt("Введите любое целое положительное число:"));
+// let sum = 0;
+
+// for (let i = 1; i <= userInput; i++) {
+// sum += i;
+// }
+
+// console.log(`Сумма чисел от 1 до ${userInput} равна ${sum}.`);
+
+// 2) Напишите цикл, который за 7 вызовов console.log выводит такой треугольник:
+
+// let someString = "";
+// for (let i = 1; i <= 7; i++) {
+//     someString = someString + "#";
+//   console.log(someString);
+// }
+
+// 3) FizzBuzz
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//     console.log('FizzBuzz');
+//     } else if (i % 3 === 0) {
+//     console.log('Fizz');
+//     } else if (i % 5 === 0) {
+//     console.log('Buzz');
+//     } else {
+//     console.log(i);
+//     }
+//     }
