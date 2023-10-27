ef0ffa4bcebaa4af5c87257415b269300cd1bb09
MahabatAnarkulova <anarkulovamahabat18@gmail.com>
2023-10-27 14:24:37 +0600

    homework2

--- main.js
+++ main.js
@@ -9,24 +9,4 @@
 
 // console.log(`Сумма чисел от 1 до ${userInput} равна ${sum}.`);
 
-// 2) Напишите цикл, который за 7 вызовов console.log выводит такой треугольник:
 
-// let someString = "";
-// for (let i = 1; i <= 7; i++) {
-//     someString = someString + "#";
-//   console.log(someString);
-// }
-
-// 3) FizzBuzz
-
-// for (let i = 1; i <= 100; i++) {
-//     if (i % 3 === 0 && i % 5 === 0) {
-//     console.log('FizzBuzz');
-//     } else if (i % 3 === 0) {
-//     console.log('Fizz');
-//     } else if (i % 5 === 0) {
-//     console.log('Buzz');
-//     } else {
-//     console.log(i);
-//     }
-//     }
